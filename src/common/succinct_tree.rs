866a0fc8fa5f7b2c4d04cd8575efef364227d428
Kevin Kaßelmann <kevin.kasselmann@arithnea.de>
2018-05-25 11:12:50 +0200

    Added is valid for succinct trees

--- src/common/succinct_tree.rs
+++ src/common/succinct_tree.rs
@@ -1,5 +1,6 @@
 use std::fmt::Debug;
 use id_tree::Tree;
+use bit_vec::BitVec;
 
 pub trait SuccinctTree<T>: Debug {
     fn is_leaf(&self, index: u64) -> bool;
@@ -7,4 +8,25 @@ pub trait SuccinctTree<T>: Debug {
     fn first_child(&self, index: u64) -> Option<u64>;
     fn next_sibling(&self, index: u64) -> Option<u64>;
     fn from_id_tree(tree: Tree<i32>) -> T;
+
+    ///  Prüft ob ein Bitvector ein gültiger SuccinctTree ist, anhand des gültige Exzesses und der Anzahl öffnender und schließender Klammern
+    fn is_valid(bitvec: BitVec) -> bool {
+        let mut excess : u64 = 0;
+        for i  in 0..bitvec.len() {
+            let x = bitvec.get(i).unwrap();
+            if x {
+                excess = excess + 1;
+            } else {
+                excess = excess - 1;
+            }
+            if excess == 0 && i < bitvec.len() {
+                false
+            }
+        }
+        if excess != 0 {
+            false
+        }
+        true
+    }
+
 }
