819c704ff2523eaec31721658b2ed3474ebf2ea8
Kevin Kaßelmann <kevin.kasselmann@arithnea.de>
2018-06-01 09:48:03 +0200

    Modified function doc

--- src/common/succinct_tree.rs
+++ src/common/succinct_tree.rs
@@ -9,8 +9,13 @@ pub trait SuccinctTree<T>: Debug {
     fn next_sibling(&self, index: u64) -> Option<u64>;
     fn from_id_tree(tree: Tree<i32>) -> T;
 
-    ///  Prüft ob ein Bitvector ein gültiger SuccinctTree ist, anhand des gültige Exzesses und
+    /// Prüft ob ein Bitvector ein gültiger SuccinctTree ist, anhand des gültige Exzesses und
     /// der Anzahl öffnender und schließender Klammern
+    ///
+    ///  # Arguments
+    ///
+    /// * `bitvec` - A bit vector.
+    ///
     fn is_valid(bitvec: &BitVec<u8>) -> bool {
         let mut excess = 0;
         for i in 0..bitvec.len() {
@@ -29,4 +34,5 @@ pub trait SuccinctTree<T>: Debug {
         }
         true
     }
+
 }
